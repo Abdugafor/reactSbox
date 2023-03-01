5ad7c47944ee5e10c9976cff486c2ac1a4cb8289
Abdugafor Desktop <gafarovabdugafur@gmail.com>
2023-03-01 19:22:48 +0500

    ddff

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import * as ReactDom from 'react-dom'
+import { Header } from './header'
+
+// window.addEventListener('load', () => {
+//     React.render(<Header/>, document.getElementById('root'))
+// })
