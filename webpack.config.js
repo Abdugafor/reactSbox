19db3a303360afa4b64189713cd7e814a88a6b6a
Abdugafor Leptop <abdugaforgafarov0111@gmail.com>
2023-03-08 12:54:27 +0500

    ...

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const NODE_ENV = process.env.NODE_ENV;
+const IS_DEV = NODE_EVN === 'development'
 module.exports = {
     resolve: {
         extensions: ['.js', '.jsx', '.ts' ,  '.tsx', '.json']
@@ -16,6 +19,14 @@ module.exports = {
             test: /\.[tj]sx?$/,
             use: ['ts-loader']
         }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: path.resolve(__dirname, 'index.html')})
+    ],
+    devServer: {
+        port: 3000,
+        open: true,
+        hot: IS_DEV
     }
 };
 
